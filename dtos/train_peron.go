2d0fea77e016bf2a1be9c1afadff500c2d7811c9
Mochammad Hanif <mochanifz@gmail.com>
2023-05-20 14:21:48 +0700

    Feat: Update train peron CRUD

--- dtos/train_peron.go
+++ dtos/train_peron.go
@@ -3,20 +3,21 @@ package dtos
 import "time"
 
 type TrainPeronInput struct {
-	TrainID uint    `json:"train_id" form:"train_id"`
-	Class   string  `json:"class" form:"class"`
-	Name    string  `json:"name" form:"name"`
-	Price   int     `json:"price" form:"Price"`
-	Status  *string `json:"status" form:"status"`
+	TrainID uint   `json:"train_id" form:"train_id" example:"1"`
+	Class   string `json:"class" form:"class" example:"Ekonomi"`
+	Name    string `json:"name" form:"name" example:"Gerbong 1"`
+	Price   int    `json:"price" form:"Price" example:"50000"`
+	Status  string `json:"status" form:"status" example:"available"`
 }
 
 type TrainPeronResponse struct {
-	TrainPeronID uint       `json:"train_peron_id" form:"train_peron_id"`
-	TrainID      uint       `json:"train_id" form:"train_id"`
-	Train        TrainInput `json:"train" form:"train"`
-	Class        string     `json:"class" form:"class"`
-	Name         string     `json:"name" form:"name"`
-	Price        int        `json:"price" form:"Price"`
-	Status       *string    `json:"status" form:"status"`
-	UpdateAt     time.Time  `json:"update_at"`
+	TrainPeronID uint                `json:"train_peron_id" example:"1"`
+	Train        TrainResponse       `json:"train"`
+	Class        string              `json:"class" example:"Ekonomi"`
+	Name         string              `json:"name" example:"Gerbong 1"`
+	Seat         []TrainSeatResponse `json:"seat"`
+	Price        int                 `json:"price" example:"50000"`
+	Status       string              `json:"status" example:"available"`
+	CreatedAt    time.Time           `json:"created_at" example:"2023-05-17T15:07:16.504+07:00"`
+	UpdatedAt    time.Time           `json:"updated_at" example:"2023-05-17T15:07:16.504+07:00"`
 }
--- models/train_peron.go
+++ models/train_peron.go
@@ -4,10 +4,10 @@ import "gorm.io/gorm"
 
 type TrainPeron struct {
 	gorm.Model
-	TrainID uint    `json:"train_id" form:"train_id"`
-	Train   Train   `gorm:"foreignKey:TrainID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"train" form:"train"`
-	Class   string  `json:"class" form:"class"`
-	Name    string  `json:"name" form:"name"`
-	Price   int     `json:"price" form:"Price"`
-	Status  *string `gorm:"type:ENUM('available','unavailable');null" json:"status" form:"status"`
+	TrainID uint
+	Train   Train  `gorm:"foreignKey:TrainID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	Class   string `gorm:"type:varchar(255)"`
+	Name    string
+	Price   int
+	Status  string `gorm:"type:ENUM('available','unavailable');default:'unavailable'"`
 }
