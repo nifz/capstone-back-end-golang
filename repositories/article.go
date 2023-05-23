0cb7de9a0ca13edf737fb479f9fdd97303d5ca2c
Almas Firdaus <76735572+AlmasFirdaus@users.noreply.github.com>
2023-05-23 15:05:35 +0700

    Feat: create article, read all article or by id, update article, delete article

---
+++ repositories/article.go
@@ -0,0 +1,63 @@
+package repositories
+
+import (
+	"back-end-golang/models"
+
+	"gorm.io/gorm"
+)
+
+type ArticleRepository interface {
+	GetAllArticles(page, limit int) ([]models.Article, int, error)
+	GetArticleByID(id uint) (models.Article, error)
+	CreateArticle(article models.Article) (models.Article, error)
+	UpdateArticle(article models.Article) (models.Article, error)
+	DeleteArticle(article models.Article) error
+}
+
+type articleRepository struct {
+	db *gorm.DB
+}
+
+func NewArticleRepository(db *gorm.DB) ArticleRepository {
+	return &articleRepository{db}
+}
+
+// Implementasi fungsi-fungsi dari interface ItemRepository
+
+func (r *articleRepository) GetAllArticles(page, limit int) ([]models.Article, int, error) {
+	var (
+		articles []models.Article
+		count    int64
+	)
+	err := r.db.Find(&articles).Count(&count).Error
+	if err != nil {
+		return articles, int(count), err
+	}
+
+	offset := (page - 1) * limit
+
+	err = r.db.Limit(limit).Offset(offset).Find(&articles).Error
+
+	return articles, int(count), err
+}
+
+func (r *articleRepository) GetArticleByID(id uint) (models.Article, error) {
+	var article models.Article
+	err := r.db.Where("id = ?", id).First(&article).Error
+	return article, err
+}
+
+func (r *articleRepository) CreateArticle(article models.Article) (models.Article, error) {
+	err := r.db.Create(&article).Error
+	return article, err
+}
+
+func (r *articleRepository) UpdateArticle(article models.Article) (models.Article, error) {
+	err := r.db.Save(&article).Error
+	return article, err
+}
+
+func (r *articleRepository) DeleteArticle(article models.Article) error {
+	err := r.db.Delete(&article).Error
+	return err
+}
---
+++ usecases/article.go
@@ -0,0 +1,222 @@
+package usecases
+
+import (
+	"back-end-golang/dtos"
+	"back-end-golang/helpers"
+	"back-end-golang/models"
+	"back-end-golang/repositories"
+	"fmt"
+)
+
+type ArticleUsecase interface {
+	GetAllArticles(page, limit int) ([]dtos.ArticleResponse, int, error)
+	GetArticleByID(id uint) (dtos.ArticleResponse, error)
+	CreateArticle(articleInput *dtos.ArticleInput) (dtos.ArticleResponse, error)
+	UpdateArticle(id uint, articleInput dtos.ArticleInput) (dtos.ArticleResponse, error)
+	DeleteArticle(id uint) error
+}
+
+type articleUsecase struct {
+	articleRepo repositories.ArticleRepository
+}
+
+func NewArticleUsecase(ArticleRepo repositories.ArticleRepository) ArticleUsecase {
+	return &articleUsecase{ArticleRepo}
+}
+
+// GetAllArticles godoc
+// @Summary      Get all articles
+// @Description  Get all articles
+// @Tags         Article
+// @Accept       json
+// @Produce      json
+// @Param page query int false "Page number"
+// @Param limit query int false "Number of items per page"
+// @Success      200 {object} dtos.GetAllArticleStatusOKResponse
+// @Failure      400 {object} dtos.BadRequestResponse
+// @Failure      401 {object} dtos.UnauthorizedResponse
+// @Failure      403 {object} dtos.ForbiddenResponse
+// @Failure      404 {object} dtos.NotFoundResponse
+// @Failure      500 {object} dtos.InternalServerErrorResponse
+// @Router       /admin/article [get]
+// @Security BearerAuth
+func (u *articleUsecase) GetAllArticles(page, limit int) ([]dtos.ArticleResponse, int, error) {
+	articles, count, err := u.articleRepo.GetAllArticles(page, limit)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	var articleResponses []dtos.ArticleResponse
+	for _, article := range articles {
+		articleResponse := dtos.ArticleResponse{
+			ArticleID:   article.ID,
+			Title:       article.Title,
+			Image:       article.Image,
+			Description: article.Description,
+			Label:       article.Label,
+			Slug:        article.Slug,
+			CreatedAt:   article.CreatedAt,
+			UpdatedAt:   article.UpdatedAt,
+		}
+		articleResponses = append(articleResponses, articleResponse)
+	}
+
+	return articleResponses, count, nil
+}
+
+// GetArticleByID godoc
+// @Summary      Get article by ID
+// @Description  Get article by ID
+// @Tags         Article
+// @Accept       json
+// @Produce      json
+// @Param id path integer true "ID article"
+// @Success      200 {object} dtos.ArticleStatusOKResponse
+// @Failure      400 {object} dtos.BadRequestResponse
+// @Failure      401 {object} dtos.UnauthorizedResponse
+// @Failure      403 {object} dtos.ForbiddenResponse
+// @Failure      404 {object} dtos.NotFoundResponse
+// @Failure      500 {object} dtos.InternalServerErrorResponse
+// @Router       /admin/article/{id} [get]
+// @Security BearerAuth
+func (u *articleUsecase) GetArticleByID(id uint) (dtos.ArticleResponse, error) {
+	var articleResponses dtos.ArticleResponse
+	article, err := u.articleRepo.GetArticleByID(id)
+	if err != nil {
+		return articleResponses, err
+	}
+	articleResponse := dtos.ArticleResponse{
+		ArticleID:   article.ID,
+		Title:       article.Title,
+		Image:       article.Image,
+		Description: article.Description,
+		Label:       article.Label,
+		Slug:        article.Slug,
+		CreatedAt:   article.CreatedAt,
+		UpdatedAt:   article.UpdatedAt,
+	}
+	return articleResponse, nil
+}
+
+// CreateArticle godoc
+// @Summary      Create a new article
+// @Description  Create a new article
+// @Tags         Article
+// @Accept       json
+// @Produce      json
+// @Param        request body dtos.ArticleInput true "Payload Body [RAW]"
+// @Success      200 {object} dtos.StationStatusOKResponse
+// @Failure      400 {object} dtos.BadRequestResponse
+// @Failure      401 {object} dtos.UnauthorizedResponse
+// @Failure      403 {object} dtos.ForbiddenResponse
+// @Failure      404 {object} dtos.NotFoundResponse
+// @Failure      500 {object} dtos.InternalServerErrorResponse
+// @Router       /admin/article [post]
+// @Security BearerAuth
+func (u *articleUsecase) CreateArticle(articleInput *dtos.ArticleInput) (dtos.ArticleResponse, error) {
+	var articleResponses dtos.ArticleResponse
+	slug := helpers.CreateSlug(articleInput.Title)
+	CreateArticle := models.Article{
+		Title:       articleInput.Title,
+		Image:       articleInput.Image,
+		Description: articleInput.Description,
+		Label:       articleInput.Label,
+		Slug:        slug,
+	}
+
+	createdArticle, err := u.articleRepo.CreateArticle(CreateArticle)
+	fmt.Println(CreateArticle)
+	if err != nil {
+		return articleResponses, err
+	}
+
+	articleResponse := dtos.ArticleResponse{
+		ArticleID:   createdArticle.ID,
+		Title:       createdArticle.Title,
+		Image:       createdArticle.Image,
+		Description: createdArticle.Description,
+		Label:       createdArticle.Label,
+		Slug:        createdArticle.Slug,
+		CreatedAt:   createdArticle.CreatedAt,
+		UpdatedAt:   createdArticle.UpdatedAt,
+	}
+	return articleResponse, nil
+}
+
+// UpdateArticle godoc
+// @Summary      Update article
+// @Description  Update article
+// @Tags         Article
+// @Accept       json
+// @Produce      json
+// @Param id path integer true "ID article"
+// @Param        request body dtos.ArticleInput true "Payload Body [RAW]"
+// @Success      200 {object} dtos.ArticleStatusOKResponse
+// @Failure      400 {object} dtos.BadRequestResponse
+// @Failure      401 {object} dtos.UnauthorizedResponse
+// @Failure      403 {object} dtos.ForbiddenResponse
+// @Failure      404 {object} dtos.NotFoundResponse
+// @Failure      500 {object} dtos.InternalServerErrorResponse
+// @Router       /admin/article [put]
+// @Security BearerAuth
+func (u *articleUsecase) UpdateArticle(id uint, articleInput dtos.ArticleInput) (dtos.ArticleResponse, error) {
+	var article models.Article
+	var articleResponse dtos.ArticleResponse
+
+	article, err := u.articleRepo.GetArticleByID(id)
+	if err != nil {
+		return articleResponse, err
+	}
+
+	slug := helpers.CreateSlug(articleInput.Title)
+
+	article.Title = articleInput.Title
+	article.Image = articleInput.Image
+	article.Description = articleInput.Description
+	article.Label = articleInput.Label
+	article.Slug = slug
+
+	article, err = u.articleRepo.UpdateArticle(article)
+
+	if err != nil {
+		return articleResponse, err
+	}
+
+	articleResponse.ArticleID = article.ID
+	articleResponse.Title = article.Title
+	articleResponse.Image = article.Image
+	articleResponse.Description = article.Description
+	articleResponse.Label = article.Label
+	articleResponse.Slug = article.Slug
+	articleResponse.CreatedAt = article.CreatedAt
+	articleResponse.UpdatedAt = article.UpdatedAt
+
+	return articleResponse, nil
+
+}
+
+// DeleteArticle godoc
+// @Summary      Delete a article
+// @Description  Delete a article
+// @Tags         Article
+// @Accept       json
+// @Produce      json
+// @Param id path integer true "ID article"
+// @Success      200 {object} dtos.StatusOKDeletedResponse
+// @Failure      400 {object} dtos.BadRequestResponse
+// @Failure      401 {object} dtos.UnauthorizedResponse
+// @Failure      403 {object} dtos.ForbiddenResponse
+// @Failure      404 {object} dtos.NotFoundResponse
+// @Failure      500 {object} dtos.InternalServerErrorResponse
+// @Router       /admin/article/{id} [delete]
+// @Security BearerAuth
+func (u *articleUsecase) DeleteArticle(id uint) error {
+	article, err := u.articleRepo.GetArticleByID(id)
+
+	if err != nil {
+		return nil
+	}
+
+	err = u.articleRepo.DeleteArticle(article)
+	return err
+}
