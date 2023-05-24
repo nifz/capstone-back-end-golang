6a1c3381702f94495d2e08ffaa400ec0324d569c
Mochammad Hanif <mochanifz@gmail.com>
2023-05-24 12:16:24 +0700

    Fix: Rerouting API

--- routes/route.go
+++ routes/route.go
@@ -26,35 +26,9 @@ func Init(e *echo.Echo, db *gorm.DB) {
 		log.Fatal("Error loading .env file")
 	}
 
-	// USER
-
 	userRepository := repositories.NewUserRepository(db)
 	userUsecase := usecases.NewUserUsecase(userRepository)
 	userController := controllers.NewUserController(userUsecase)
-
-	api := e.Group("/api/v1")
-	api.POST("/login", userController.UserLogin)
-	api.POST("/register", userController.UserRegister)
-
-	user := api.Group("/user")
-	user.Use(middlewares.JWTMiddleware, middlewares.RoleMiddleware("user"))
-	user.Any("", userController.UserCredential)
-	user.PATCH("/update-information", userController.UserUpdateInformation)
-	user.PUT("/update-password", userController.UserUpdatePassword)
-	user.PUT("/update-profile", userController.UserUpdateProfile)
-	user.PUT("/update-photo-profile", userController.UserUpdatePhotoProfile)
-	user.DELETE("/delete-photo-profile", userController.UserDeletePhotoProfile)
-
-	historySearchRepository := repositories.NewHistorySearchRepository(db)
-	historySearchUsecase := usecases.NewHistorySearchUsecase(historySearchRepository, userRepository)
-	historySearchController := controllers.NewHistorySearchController(historySearchUsecase)
-
-	user.GET("/history-search", historySearchController.HistorySearchGetById)
-	user.POST("/history-search", historySearchController.HistorySearchCreate)
-	user.PUT("/history-search", historySearchController.HistorySearchUpdate)
-	user.DELETE("/history-search", historySearchController.HistorySearchDelete)
-
-	// ADMIN
 
 	stationRepository := repositories.NewStationRepository(db)
 	stationUsecase := usecases.NewStationUsecase(stationRepository)
@@ -64,9 +38,9 @@ func Init(e *echo.Echo, db *gorm.DB) {
 	trainUsecase := usecases.NewTrainUsecase(trainRepository)
 	trainController := controllers.NewTrainController(trainUsecase)
 
-	trainPeronRepository := repositories.NewTrainPeronRepository(db)
-	trainPeronUsecase := usecases.NewTrainPeronUsecase(trainPeronRepository)
-	trainPeronController := controllers.NewTrainPeronController(trainPeronUsecase)
+	trainCarriageRepository := repositories.NewTrainCarriageRepository(db)
+	trainCarriageUsecase := usecases.NewTrainCarriageUsecase(trainCarriageRepository, trainRepository)
+	trainCarriageController := controllers.NewTrainCarriageController(trainCarriageUsecase)
 
 	reservationRepository := repositories.NewReservationRepository(db)
 	reservationImageRepository := repositories.NewReservationImageRepository(db)
@@ -81,25 +55,58 @@ func Init(e *echo.Echo, db *gorm.DB) {
 	recommendationUsecase := usecases.NewRecommendationUsecase(recommendationRepository)
 	recommendationController := controllers.NewRecommendationController(recommendationUsecase)
 
+	historySearchRepository := repositories.NewHistorySearchRepository(db)
+	historySearchUsecase := usecases.NewHistorySearchUsecase(historySearchRepository, userRepository)
+	historySearchController := controllers.NewHistorySearchController(historySearchUsecase)
+
+	api := e.Group("/api/v1")
+
+	// USER
+	api.POST("/login", userController.UserLogin)
+	api.POST("/register", userController.UserRegister)
+
+	user := api.Group("/user")
+	user.Use(middlewares.JWTMiddleware, middlewares.RoleMiddleware("user"))
+
+	// user account
+	user.Any("", userController.UserCredential)
+	user.PATCH("/update-information", userController.UserUpdateInformation)
+	user.PUT("/update-password", userController.UserUpdatePassword)
+	user.PUT("/update-profile", userController.UserUpdateProfile)
+	user.PUT("/update-photo-profile", userController.UserUpdatePhotoProfile)
+	user.DELETE("/delete-photo-profile", userController.UserDeletePhotoProfile)
+
+	// train ka
+	user.GET("/train/search", trainController.SearchTrainAvailable)
+
+	user.GET("/history-search", historySearchController.HistorySearchGetAll)
+	user.POST("/history-search", historySearchController.HistorySearchCreate)
+	user.DELETE("/history-search/:id", historySearchController.HistorySearchDelete)
+
+	// ADMIN
+
 	admin := api.Group("/admin")
 	admin.Use(middlewares.JWTMiddleware, middlewares.RoleMiddleware("admin"))
+
+	// crud station
 	admin.GET("/station", stationController.GetAllStations)
 	admin.GET("/station/:id", stationController.GetStationByID)
 	admin.PUT("/station/:id", stationController.UpdateStation)
 	admin.POST("/station", stationController.CreateStation)
 	admin.DELETE("/station/:id", stationController.DeleteStation)
 
+	// crud train
 	admin.GET("/train", trainController.GetAllTrains)
 	admin.GET("/train/:id", trainController.GetTrainByID)
 	admin.PUT("/train/:id", trainController.UpdateTrain)
 	admin.POST("/train", trainController.CreateTrain)
 	admin.DELETE("/train/:id", trainController.DeleteTrain)
 
-	admin.GET("/train-peron", trainPeronController.GetAllTrainPerons)
-	admin.GET("/train-peron/:id", trainPeronController.GetTrainPeronByID)
-	admin.PUT("/train-peron/:id", trainPeronController.UpdateTrainPeron)
-	admin.POST("/train-peron", trainPeronController.CreateTrainPeron)
-	admin.DELETE("/train-peron/:id", trainPeronController.DeleteTrainPeron)
+	admin.GET("/train-carriage", trainCarriageController.GetAllTrainCarriages)
+	admin.GET("/train-carriage/:id", trainCarriageController.GetTrainCarriageByID)
+	admin.PUT("/train-carriage/:id", trainCarriageController.UpdateTrainCarriage)
+	admin.POST("/train-carriage", trainCarriageController.CreateTrainCarriage)
+	admin.DELETE("/train-carriage/:id", trainCarriageController.DeleteTrainCarriage)
 
 	admin.GET("/article", articleController.GetAllArticles)
 	admin.GET("/article/:id", articleController.GetArticleByID)
