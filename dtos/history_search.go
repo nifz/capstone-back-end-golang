ae25d73011cd3919678d7d46b937cfcba8ea4cd5
Mochammad Hanif <mochanifz@gmail.com>
2023-05-24 12:14:04 +0700

    Fix: Logic feature history search

--- dtos/history_search.go
+++ dtos/history_search.go
@@ -1,21 +1,11 @@
 package dtos
 
-type HistorySearchCreateInput struct {
-	UserID uint   `form:"user_id" json:"user_id" binding:"required"`
-	Name   string `form:"name" json:"name" binding:"required"`
+type HistorySearchInput struct {
+	Name string `form:"name" json:"name" binding:"required"`
 }
 
-type HistorySearchCreateResponse struct {
-	UserID uint   `json:"user_id"`
-	Name   string `json:"name"`
-}
-
-type HistorySearchUpdateInput struct {
-	UserID uint   `form:"user_id" json:"user_id" binding:"required"`
-	Name   string `form:"name" json:"name" binding:"required"`
-}
-
-type HistorySearchUpdateResponse struct {
+type HistorySearchResponse struct {
+	ID     uint   `json"id"`
 	UserID uint   `json:"user_id"`
 	Name   string `json:"name"`
 }
